0bb7793664cb8fcad13b8e64519457a369f50dbc
Jack700103 <wqzl0852@leeds.ac.uk>
2025-12-03 19:21:40 +0000

    data

--- task2/vpet.py
+++ task2/vpet.py
@@ -1,48 +1,32 @@
 class VirtualPet:
-    def __init__(self, name, energy=10, hunger=0):
+    def __init__(self, name, tiredness=0, fullness=10):
         self.name = name
-        self.energy = energy
-        self.hunger = hunger
-
+        self.tiredness = tiredness  
+        self.fullness = fullness    
+    
     def play(self):
-        if self.energy < 2:
-            print(f"{self.name} is too tired to play!")
-        else:
-            self.energy -= 2
-            self.hunger += 2
-
+        if self.tiredness >= 8:  
+            return f"{self.name} is too tired to play!"
+        self.tiredness += 2
+        self.fullness -= 2 
+        return f"{self.name} played and is now less tired!"
+    
     def feed(self):
-        self.hunger -= 3
-        if self.hunger < 0:
-            print(f"{self.name} is overfed!")
-            self.hunger = 0
-
+        if self.fullness >= 15:  
+            self.fullness = 15  
+            return f"{self.name} is overfed!"
+        self.fullness += 5
+        return f"{self.name} ate and is now more full!"
+    
     def sleep(self):
-        self.energy += 10
-
+        self.tiredness = max(0, self.tiredness - 10) 
+        return f"{self.name} slept and is now refreshed!"
+    
     def __str__(self):
-        return f"{self.name} has {self.energy} energy points and hunger level {self.hunger}"
-
+        return f"{self.name}: Tiredness={self.tiredness}, Fullness={self.fullness}"
+    
     def __eq__(self, other):
         return (isinstance(other, VirtualPet) and
                 self.name == other.name and
-                self.energy == other.energy and
-                self.hunger == other.hunger)
-    
-
-class Tamagotchi:
-    def __init__(self):
-        self.tiredness = 0
-        self.fullness = 10
-    
-    def play(self):
-        if self.tiredness >= 8:
-            return "Timmy is too tired to play!"  
-        self.tiredness += 2
-        return "Timmy played and is now less tired!"
-    
-    def feed(self):
-        if self.fullness >= 15: 
-            return "Timmy is overfed!"  
-        self.fullness += 5
-        return "Timmy ate and is now more full!"
+                self.tiredness == other.tiredness and
+                self.fullness == other.fullness)
